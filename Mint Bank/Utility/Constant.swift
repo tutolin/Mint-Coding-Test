f0e258a18adcdfba6c68b702cb6185d1f194d182
Aboyeji Tolulope <tolulope.aboyeji@fundall.io>
2020-11-29 14:44:08 +0100

    created object class for network layer static data

---
+++ Mint Bank/Utility/Constant.swift
@@ -0,0 +1,18 @@
+//
+//  Constant.swift
+//  Mint Bank
+//
+//  Created by Aboyeji Tolulope on 29/11/2020.
+//
+
+import Foundation
+
+struct HeaderConstant {
+    
+    static let applicationJson = "application/json"
+    static let contentType = "Content-Type"
+    static let tokenHeader = "Authorization"
+    static let accept = "Accept"
+    
+    
+}
