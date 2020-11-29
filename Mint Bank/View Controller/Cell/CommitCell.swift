15c10e7dd5a1a3f3bb8eb94ffec387918761a75c
Aboyeji Tolulope <tolulope.aboyeji@fundall.io>
2020-11-29 13:37:18 +0100

    added commitcell view

---
+++ Mint Bank/View Controller/Cell/CommitCell.swift
@@ -0,0 +1,23 @@
+//
+//  CommitCell.swift
+//  Mint Bank
+//
+//  Created by Fundall on 29/11/2020.
+//
+
+import UIKit
+
+class CommitCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Mint Bank/View Controller/CommitViewController.swift
+++ Mint Bank/View Controller/CommitViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class CommitViewController: UIViewController {
+    
+    @IBOutlet private weak var commitTableView: UITableView!
+    @IBOutlet private weak var errorView: UIView!
+    @IBOutlet private weak var errorLabel: UILabel!
+    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
