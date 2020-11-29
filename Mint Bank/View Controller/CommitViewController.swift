b7f1a2537bbe15fa948601f2251603858362d746
Aboyeji Tolulope <tolulope.aboyeji@fundall.io>
2020-11-29 15:08:04 +0100

    added tableview datasource and delegate to commitviewcontroller

--- Mint Bank/View Controller/CommitViewController.swift
+++ Mint Bank/View Controller/CommitViewController.swift
@@ -13,12 +13,42 @@ class CommitViewController: UIViewController {
     @IBOutlet private weak var errorView: UIView!
     @IBOutlet private weak var errorLabel: UILabel!
     @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
+    
+    var commits: [CommitModel] = []
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        commitTableView.delegate = self
+        commitTableView.dataSource = self
+        commitTableView.tableFooterView = UIView()
+
+       
+        
+
         // Do any additional setup after loading the view.
     }
 
 
 }
+
+extension RepoViewController:  UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if commits.count > 25 {
+            
+            return 25
+        }
+        return commits.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+  
+    }
+    
+    
+}
+
+
 
